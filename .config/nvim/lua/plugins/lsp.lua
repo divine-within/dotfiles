7129ee1dd531cf367f0d46cd731d3ef1c3feb994
divine-yume <13299392+divine-yume@users.noreply.github.com>
2023-03-01 20:45:11 +0100

    Added LSPSaga plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -100,6 +100,101 @@ return {
         }
     },
 
+    { "glepnir/lspsaga.nvim",
+        lazy = true,
+        event = "BufRead",
+        config = function()
+          require("lspsaga").setup({})
+
+        -- LSP finder - Find the symbol's definition
+        -- If there is no definition, it will instead be hidden
+        -- When you use an action in finder like "open vsplit",
+        -- you can use <C-t> to jump back
+        vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+        -- Code action
+        vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+        -- Rename all occurrences of the hovered word for the entire file
+        vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+        -- Rename all occurrences of the hovered word for the selected files
+        vim.keymap.set("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+
+        -- Peek definition
+        -- You can edit the file containing the definition in the floating window
+        -- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
+        -- It also supports tagstack
+        -- Use <C-t> to jump back
+        vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+
+        -- Go to definition
+        vim.keymap.set("n","gd", "<cmd>Lspsaga goto_definition<CR>")
+
+        -- Peek type definition
+        -- You can edit the file containing the type definition in the floating window
+        -- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
+        -- It also supports tagstack
+        -- Use <C-t> to jump back
+        vim.keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
+
+        -- Go to type definition
+        vim.keymap.set("n","gt", "<cmd>Lspsaga goto_type_definition<CR>")
+
+
+        -- Show line diagnostics
+        -- You can pass argument ++unfocus to
+        -- unfocus the show_line_diagnostics floating window
+        vim.keymap.set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+
+        -- Show cursor diagnostics
+        -- Like show_line_diagnostics, it supports passing the ++unfocus argument
+        vim.keymap.set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+        -- Show buffer diagnostics
+        vim.keymap.set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+        -- Diagnostic jump
+        -- You can use <C-o> to jump back to your previous location
+        vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+        vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+        -- Diagnostic jump with filters such as only jumping to an error
+        vim.keymap.set("n", "[E", function()
+          require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+        end)
+        vim.keymap.set("n", "]E", function()
+          require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+        end)
+
+        -- Toggle outline
+        vim.keymap.set("n","<leader>o", "<cmd>Lspsaga outline<CR>")
+
+        -- Hover Doc
+        -- If there is no hover doc,
+        -- there will be a notification stating that
+        -- there is no information available.
+        -- To disable it just use ":Lspsaga hover_doc ++quiet"
+        -- Pressing the key twice will enter the hover window
+        vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+
+        -- If you want to keep the hover window in the top right hand corner,
+        -- you can pass the ++keep argument
+        -- Note that if you use hover with ++keep, pressing this key again will
+        -- close the hover window. If you want to jump to the hover window
+        -- you should use the wincmd command "<C-w>w"
+        vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+
+        -- Call hierarchy
+        vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+        vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+        -- Floating terminal
+        vim.keymap.set({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
+        end,
+        dependencies = { {"nvim-tree/nvim-web-devicons"} }
+    },
+
     { "jose-elias-alvarez/null-ls.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
@@ -125,11 +220,6 @@ return {
                 },
                 on_attach = function(client, bufnr)
                     if client.supports_method("textDocument/formatting") then
-                        vim.api.nvim_buf_create_user_command(bufnr, "LspFormatting", function()
-                            -- or vim.lsp.buf.formatting(bufnr) on 0.8
-                            vim.lsp.buf.format({ bufnr = bufnr })
-                        end, {})
-
                         -- you can leave this out if your on_attach is unique to null-ls,
                         -- but if you share it with multiple servers, you'll want to keep it
                         vim.api.nvim_clear_autocmds({
@@ -140,7 +230,9 @@ return {
                         vim.api.nvim_create_autocmd("BufWritePre", {
                             group = augroup,
                             buffer = bufnr,
-                            command = "undojoin | LspFormatting",
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = bufnr })
+                            end,
                         })
                     end
                 end,
