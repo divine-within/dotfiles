e6dc558db6b99af1fa1881704d9b72843aecb1fd
divine-yume <13299392+divine-yume@users.noreply.github.com>
2023-03-07 22:55:33 +0100

    Removed Super Tab
    Fix: Luasnip config, hot reload

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -1,154 +1,110 @@
 return {
-    { "onsails/lspkind-nvim",
-        config = function()
-            local lspkind = require("lspkind")
+	{
+		"onsails/lspkind-nvim",
+		config = function()
+			local lspkind = require("lspkind")
 
-            lspkind.init({
-                -- DEPRECATED (use mode instead): enables text annotations
-                --
-                -- default: true
-                -- with_text = true,
+			lspkind.init({
+				mode = "symbol_text",
+				preset = "codicons",
+				symbol_map = {
+					Text = "",
+					Method = "",
+					Function = "",
+					Constructor = "",
+					Field = "ﰠ",
+					Variable = "",
+					Class = "ﴯ",
+					Interface = "",
+					Module = "",
+					Property = "ﰠ",
+					Unit = "塞",
+					Value = "",
+					Enum = "",
+					Keyword = "",
+					Snippet = "",
+					Color = "",
+					File = "",
+					Reference = "",
+					Folder = "",
+					EnumMember = "",
+					Constant = "",
+					Struct = "פּ",
+					Event = "",
+					Operator = "",
+					TypeParameter = "",
+				},
+			})
+		end,
+	},
 
-                -- defines how annotations are shown
-                -- default: symbol
-                -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-                mode = 'symbol_text',
-                -- default symbol map
-                -- can be either 'default' (requires nerd-fonts font) or
-                -- 'codicons' for codicon preset (requires vscode-codicons font)
-                --
-                -- default: 'default'
-                preset = 'codicons',
-                -- override preset symbols
-                --
-                -- default: {}
-                symbol_map = {
-                    Text = "",
-                    Method = "",
-                    Function = "",
-                    Constructor = "",
-                    Field = "ﰠ",
-                    Variable = "",
-                    Class = "ﴯ",
-                    Interface = "",
-                    Module = "",
-                    Property = "ﰠ",
-                    Unit = "塞",
-                    Value = "",
-                    Enum = "",
-                    Keyword = "",
-                    Snippet = "",
-                    Color = "",
-                    File = "",
-                    Reference = "",
-                    Folder = "",
-                    EnumMember = "",
-                    Constant = "",
-                    Struct = "פּ",
-                    Event = "",
-                    Operator = "",
-                    TypeParameter = ""
-                },
-            })
-        end
-    },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "onsails/lspkind-nvim", "L3MON4D3/LuaSnip" },
+		config = function()
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
 
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = "L3MON4D3/LuaSnip",
-        config = function()
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
-            local luasnip = require("luasnip")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = {
+					["<Up>"] = cmp.mapping.select_prev_item(),
+					["<Down>"] = cmp.mapping.select_next_item(),
+					["<Left>"] = cmp.mapping.select_prev_item(),
+					["<Right>"] = cmp.mapping.select_next_item(),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+				},
+				sources = cmp.config.sources({
+					-- ordered by priority
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "luasnip" },
+					{ name = "nvim_lua" },
+				}),
+				formatting = {
+					format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 }),
+				},
+			})
 
-            local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and
-                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-            end
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = {
-                    ["<Up>"] = cmp.mapping.select_prev_item(),
-                    ["<Down>"] = cmp.mapping.select_next_item(),
-                    ["<Left>"] = cmp.mapping.select_prev_item(),
-                    ["<Right>"] = cmp.mapping.select_next_item(),
-                    ["<C-b>"] = cmp.mapping.scroll_docs( -4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Insert,
-                        select = true,
-                    }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                            -- they way you will only jump inside the snippet region
-                        elseif luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable( -1) then
-                            luasnip.jump( -1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" })
-                },
-                sources = cmp.config.sources({
-                    -- ordered by priority
-                    { name = "nvim_lsp" },
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "path" },
-                    { name = "buffer" },
-                    { name = "luasnip" },
-                    { name = "nvim_lua" },
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({ wirth_text = false, maxwidth = 50 })
-                }
-            })
+			cmp.setup.cmdline({ ":" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "cmdline" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "buffer" }
-                }
-            })
-
-            cmp.setup.cmdline({ ":" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "cmdline" },
-                    { name = "path" }
-                }
-            })
-        end
-    },
-
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
-    "hrsh7th/cmp-path",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/cmp-nvim-lua",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-nvim-lsp-signature-help",
+	"hrsh7th/cmp-path",
 }
--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,28 +1,64 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        lazy = true,
-        dependencies = { "saadparwaiz1/cmp_luasnip" },
-        config = function()
-            local ls = require('luasnip')
-            local types = require('luasnip.util.types')
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "saadparwaiz1/cmp_luasnip" },
+		version = "v1.*",
+		config = function()
+			local ls = require("luasnip")
+			local types = require("luasnip.util.types")
 
-            ls.config.set_config({
-                history = true,
-                updateevents = "TextChanged, TextChangedI",
-                autosnippets = true,
-                ext_opts = {
-                    [types.choiceNode] = {
-                        active = {
-                            virt_text = { { "<--", "Error" } }
-                        }
-                    }
-                }
-            })
+			ls.setup({
+				history = true,
+				updateevents = "TextChanged, TextChangedI",
+				autosnippets = true,
+				ext_opts = {
+					[types.choiceNode] = {
+						active = {
+							virt_text = { { "", "Info" } },
+						},
+					},
+				},
+			})
 
-            require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets" })
-            -- Resource snippets
-            vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/plugins/luasnip.lua<CR>")
-        end,
-    },
+			-- Jump to the next item
+			vim.keymap.set("i", "<c-j>", function()
+				if ls.expand_or_locally_jumpable() then
+					ls.expand_or_jump()
+				end
+			end)
+
+			-- Jump to the previous item
+			vim.keymap.set("i", "<c-k>", function()
+				if ls.jumpable(-1) then
+					ls.jump(-1)
+				end
+			end)
+
+			-- Scroll forward in a list of choices
+			vim.keymap.set("i", "<c-l>", function()
+				if ls.choice_active() then
+					ls.change_choice(1)
+				end
+			end)
+
+			-- Scroll backwards in a list of choices
+			vim.keymap.set("i", "<c-h>", function()
+				if ls.choice_active() then
+					ls.change_choice(-1)
+				end
+			end)
+
+			function load_snippets()
+				require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/lua/snippets" })
+			end
+
+			-- Initial call
+			load_snippets()
+
+			-- Resource snippets
+			vim.keymap.set("n", "<leader><leader>s", function()
+				load_snippets()
+			end)
+		end,
+	},
 }
