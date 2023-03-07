e8d8737f3972804e8ff2731a94d1be5f12c8e1e4
divine-yume <13299392+divine-yume@users.noreply.github.com>
2023-03-07 23:03:45 +0100

    DAP: Added Python support, Fix: DAP config
    DAPUI: Improved layout

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,40 +1,92 @@
 return {
-    'mfussenegger/nvim-dap',
-    { 'leoluz/nvim-dap-go',
-        dependencies = { 'mfussenegger/nvim-dap', 'rcarriga/nvim-dap-ui' },
-        config = function()
-            require('dap-go').setup()
-            local dap = require('dap')
-            local dapui = require("dapui")
+	{
+		"mfussenegger/nvim-dap",
+		lazy = true,
+		dependencies = { "rcarriga/nvim-dap-ui" },
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
 
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
 
-            vim.keymap.set('n', '<Leader>dui', function() dap.continue() end)
+			vim.keymap.set("n", "<Leader>dui", function()
+				dap.continue()
+			end)
 
-            vim.keymap.set('n', '<F5>', function() dap.continue() end)
-            vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-            vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-            vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-            vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
-            vim.keymap.set('n', '<Leader>lp',
-                function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-            vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-            vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
-            vim.keymap.set('n', '<Leader>dt', function() require('dap-go').debug_test() end)
+			vim.keymap.set("n", "<F5>", function()
+				dap.continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				dap.step_over()
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				dap.step_into()
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				dap.step_out()
+			end)
+			vim.keymap.set("n", "<Leader>b", function()
+				dap.toggle_breakpoint()
+			end)
+			vim.keymap.set("n", "<Leader>lp", function()
+				dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end)
+			vim.keymap.set("n", "<Leader>dr", function()
+				dap.repl.open()
+			end)
+			vim.keymap.set("n", "<Leader>dl", function()
+				dap.run_last()
+			end)
 
-            local sign = vim.fn.sign_define
+			local sign = vim.fn.sign_define
 
-            sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-            sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-            sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
-        end,
-    },
+			sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+			sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+			sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+		end,
+	},
+
+	{},
+
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dap_go = require("dap-go")
+
+			dap_go.setup()
+
+			vim.keymap.set("n", "<Leader>dt", function()
+				dap_go.debug_test()
+			end)
+		end,
+	},
+
+	{
+		"mfussenegger/nvim-dap-python",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dap_python = require("dap-python")
+
+			dap_python.setup()
+
+			vim.keymap.set("n", "<Leader>ptm", function()
+				dap_python.test_method()
+			end)
+			vim.keymap.set("n", "<Leader>ptc", function()
+				dap_python.test_class()
+			end)
+			vim.keymap.set("n", "<Leader>pds", function()
+				dap_python.debug_selection()
+			end)
+		end,
+	},
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,143 +1,187 @@
 return {
-    -- status line
-    { "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-        opts = {
-            options = {
-                theme = "catppuccin"
-            },
-            sections = {
-                lualine_c = {
-                    {
-                        "filename",
-                        file_status = true, -- displays file status (readonly status, modified status)
-                        path = 1, -- relative path
-                        shorting_target = 40 -- Shortens path to leave 40 space in the window
-                    },
-                },
-            }
-        }
-    },
+	-- status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+		opts = {
+			options = {
+				theme = "catppuccin",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- displays file status (readonly status, modified status)
+						path = 1, -- relative path
+						shorting_target = 40, -- Shortens path to leave 40 space in the window
+					},
+				},
+			},
+		},
+	},
 
-    -- ui improvements
-    {
-        "stevearc/dressing.nvim",
-        config = true,
-    },
+	-- ui improvements
+	{
+		"stevearc/dressing.nvim",
+		config = true,
+	},
 
-    -- Hex code colorizer
-    { 'norcalli/nvim-colorizer.lua',
-        config = true,
-    },
+	-- Hex code colorizer
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = true,
+	},
 
-    -- tab ui improvments
-    { "akinsho/bufferline.nvim",
-        version = "v3.*",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            options = {
-                mode = "tabs",
-                indicator = { style = "underline" },
-                tab_size = 15,
-                offsets = {
-                    {
-                        filetype = "Neotree",
-                        text = "File Explorer",
-                        highlight = "Directory",
-                        separator = true -- use a "true" to enable the default, or set your own character
-                    }
-                },
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                separator_style = "none",
-                always_show_bufferline = true,
-                enforce_regular_tabs = true,
-                highlights = require("catppuccin.groups.integrations.bufferline").get()
-            }
-        },
-    },
+	-- tab ui improvments
+	{
+		"akinsho/bufferline.nvim",
+		version = "v3.*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				mode = "tabs",
+				indicator = { style = "underline" },
+				tab_size = 15,
+				offsets = {
+					{
+						filetype = "Neotree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true, -- use a "true" to enable the default, or set your own character
+					},
+				},
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				separator_style = "none",
+				always_show_bufferline = true,
+				enforce_regular_tabs = true,
+				highlights = require("catppuccin.groups.integrations.bufferline").get(),
+			},
+		},
+	},
 
-    -- quickfix improvements
-    --
-    -- <Tab> to select items.
-    -- zn to keep selected items.
-    -- zN to filter selected items.
-    -- zf to fuzzy search items.
-    { "junegunn/fzf",
-        build = function()
-            vim.fn["fzf#install"]()
-        end
-    },
+	-- quickfix improvements
+	--
+	-- <Tab> to select items.
+	-- zn to keep selected items.
+	-- zN to filter selected items.
+	-- zf to fuzzy search items.
+	{
+		"junegunn/fzf",
+		build = function()
+			vim.fn["fzf#install"]()
+		end,
+	},
 
-    { "kevinhwang91/nvim-bqf", ft = "qf" },
+	{ "kevinhwang91/nvim-bqf", ft = "qf" },
 
-    -- window bar breadcrumbs
-    {
-        "SmiteshP/nvim-navic",
-        dependencies = "neovim/nvim-lspconfig",
-        opts = {
-            highlight = true,
-        }
-    },
+	-- window bar breadcrumbs
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = "neovim/nvim-lspconfig",
+		opts = {
+			highlight = true,
+		},
+	},
 
-    -- Debugging
-    { "rcarriga/nvim-dap-ui",
-        lazy = true,
-        dependencies = { "mfussenegger/nvim-dap" },
-        config = true,
-    },
+	-- Debugging
+	{
+		"rcarriga/nvim-dap-ui",
+		lazy = true,
+		config = function(_, opts)
+			require("dapui").setup(opts)
+		end,
+		opts = {
+			layouts = {
+				{
+					elements = {
+						-- Provide as ID strings or tables with "id" and "size" keys
+						{
+							id = "scopes",
+							size = 0.25, -- Can be float or integer > 1
+						},
+						{ id = "breakpoints", size = 0.25 },
+						{ id = "stacks", size = 0.25 },
+						{ id = "watches", size = 0.25 },
+					},
+					size = 40,
+					position = "left",
+				},
+				{ elements = { "repl" }, size = 10, position = "bottom" },
+			},
+			controls = {
+				enabled = true,
+				-- Display controls in this session
+				element = "repl",
+				icons = {
+					pause = "",
+					play = "",
+					run_last = "↻",
+					step_back = "",
+					step_into = "",
+					step_out = "",
+					step_over = "",
+					terminate = "ﱢ",
+				},
+			},
+		},
+	},
 
-    -- Git
-    { "lewis6991/gitsigns.nvim",
-        opts = {
-              signs = {
-                add          = { text = '│' },
-                change       = { text = '│' },
-                delete       = { text = '_' },
-                topdelete    = { text = '‾' },
-                changedelete = { text = '~' },
-                untracked    = { text = '┆' },
-              },
-              signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-              numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-              linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-              word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-              watch_gitdir = {
-                interval = 1000,
-                follow_files = true
-              },
-              attach_to_untracked = true,
-              current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-              current_line_blame_opts = {
-                virt_text = true,
-                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                delay = 1000,
-                ignore_whitespace = false,
-              },
-              current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-              sign_priority = 6,
-              update_debounce = 100,
-              status_formatter = nil, -- Use default
-              max_file_length = 40000, -- Disable if file is longer than this (in lines)
-              preview_config = {
-                -- Options passed to nvim_open_win
-                border = 'single',
-                style = 'minimal',
-                relative = 'cursor',
-                row = 0,
-                col = 1
-              },
-              yadm = {
-                enable = false
-              },
-            }
-    },
+	-- Git
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = true,
+		event = { "CursorHold", "CursorHoldI" },
+		opts = {
+			signs = {
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				interval = 1000,
+				follow_files = true,
+			},
+			attach_to_untracked = true,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+			yadm = {
+				enable = false,
+			},
+		},
+	},
 
-    { "lukas-reineke/indent-blankline.nvim",
-        opts = {
-            indent_blankline_use_treesitter = true,
-            show_end_of_line = false,
-            show_current_context = true,
-        }
-    },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			indent_blankline_use_treesitter = true,
+			show_end_of_line = false,
+			show_current_context = true,
+		},
+	},
 }
