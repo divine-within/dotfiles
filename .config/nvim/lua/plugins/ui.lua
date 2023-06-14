acdc2e02a654b8d9734c4de12d8dc437d44b4643
divine-within <13299392+divine-yume@users.noreply.github.com>
2023-06-14 19:00:35 +0200

    Replacement: Status line

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,23 +1,72 @@
 return {
 	-- status line
 	{
-		"nvim-lualine/lualine.nvim",
+		"feline-nvim/feline.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-		opts = {
-			options = {
-				theme = "catppuccin",
-			},
-			sections = {
-				lualine_c = {
-					{
-						"filename",
-						file_status = true, -- displays file status (readonly status, modified status)
-						path = 1, -- relative path
-						shorting_target = 40, -- Shortens path to leave 40 space in the window
+		config = function()
+			local clrs = require("catppuccin.palettes").get_palette()
+			local ctp_feline = require("catppuccin.groups.integrations.feline")
+			local U = require("catppuccin.utils.colors")
+			local latte = require("catppuccin.palettes").get_palette("latte")
+
+			ctp_feline.setup({
+				assets = {
+					left_separator = "",
+					right_separator = "",
+					mode_icon = "",
+					dir = "󰉖",
+					file = "󰈙",
+					lsp = {
+						server = "󰅡",
+						error = "",
+						warning = "",
+						info = "",
+						hint = "",
+					},
+					git = {
+						branch = "",
+						added = "",
+						changed = "",
+						removed = "",
 					},
 				},
-			},
-		},
+				sett = {
+					text = U.vary_color({ latte = latte.base }, clrs.surface0),
+					bkg = U.vary_color({ latte = latte.crust }, clrs.surface0),
+					diffs = clrs.mauve,
+					extras = clrs.overlay1,
+					curr_file = clrs.maroon,
+					curr_dir = clrs.flamingo,
+					show_modified = true, -- show if the file has been modified
+				},
+				mode_colors = {
+					["n"] = { "NORMAL", clrs.lavender },
+					["no"] = { "N-PENDING", clrs.lavender },
+					["i"] = { "INSERT", clrs.green },
+					["ic"] = { "INSERT", clrs.green },
+					["t"] = { "TERMINAL", clrs.green },
+					["v"] = { "VISUAL", clrs.flamingo },
+					["V"] = { "V-LINE", clrs.flamingo },
+					["�"] = { "V-BLOCK", clrs.flamingo },
+					["R"] = { "REPLACE", clrs.maroon },
+					["Rv"] = { "V-REPLACE", clrs.maroon },
+					["s"] = { "SELECT", clrs.maroon },
+					["S"] = { "S-LINE", clrs.maroon },
+					["�"] = { "S-BLOCK", clrs.maroon },
+					["c"] = { "COMMAND", clrs.peach },
+					["cv"] = { "COMMAND", clrs.peach },
+					["ce"] = { "COMMAND", clrs.peach },
+					["r"] = { "PROMPT", clrs.teal },
+					["rm"] = { "MORE", clrs.teal },
+					["r?"] = { "CONFIRM", clrs.mauve },
+					["!"] = { "SHELL", clrs.green },
+				},
+			})
+
+			require("feline").setup({
+				components = ctp_feline.get(),
+			})
+		end,
 	},
 
 	-- ui improvements
