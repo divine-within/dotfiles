7ca594058135708071b6186d1a72cad83c2f6614
divine-within <13299392+divine-yume@users.noreply.github.com>
2023-06-17 18:55:34 +0200

    Replacement: Navic - Dropbar

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -8,6 +8,7 @@ return {
 			local ctp_feline = require("catppuccin.groups.integrations.feline")
 			local U = require("catppuccin.utils.colors")
 			local latte = require("catppuccin.palettes").get_palette("latte")
+			--local icons = require("utils.icons")
 
 			ctp_feline.setup({
 				assets = {
@@ -77,7 +78,7 @@ return {
 
 	-- Hex code colorizer
 	{
-		"norcalli/nvim-colorizer.lua",
+		"NvChad/nvim-colorizer.lua",
 		config = true,
 	},
 
@@ -125,6 +126,7 @@ return {
 	{ "kevinhwang91/nvim-bqf", ft = "qf" },
 
 	-- window bar breadcrumbs
+	--[[
 	{
 		"SmiteshP/nvim-navic",
 		dependencies = "neovim/nvim-lspconfig",
@@ -132,6 +134,9 @@ return {
 			highlight = true,
 		},
 	},
+    --]]
+
+	{ "Bekaboo/dropbar.nvim" },
 
 	-- Debugging
 	{
@@ -176,6 +181,76 @@ return {
 		},
 	},
 
+	{
+		"simrat39/symbols-outline.nvim",
+		config = function(_, opts)
+			require("symbols-outline").setup(opts)
+			vim.keymap.set("n", "<leader>so", "<cmd>SymbolsOutline<CR>", { desc = "SymbolsOutline: Toggle" })
+		end,
+		opts = {
+			highlight_hovered_item = true,
+			show_guides = true,
+			auto_preview = false,
+			position = "right",
+			relative_width = true,
+			width = 25,
+			auto_close = false,
+			show_numbers = false,
+			show_relative_numbers = false,
+			show_symbol_details = true,
+			preview_bg_highlight = "Pmenu",
+			autofold_depth = nil,
+			auto_unfold_hover = true,
+			fold_markers = { "", "" },
+			wrap = false,
+			keymaps = { -- These keymaps can be a string or a table for multiple keys
+				close = { "<Esc>", "q" },
+				goto_location = "<Cr>",
+				focus_location = "o",
+				hover_symbol = "<C-space>",
+				toggle_preview = "K",
+				rename_symbol = "r",
+				code_actions = "a",
+				fold = "e",
+				unfold = "e",
+				fold_all = "W",
+				unfold_all = "E",
+				fold_reset = "R",
+			},
+			lsp_blacklist = {},
+			symbol_blacklist = {},
+			symbols = {
+				File = { icon = "", hl = "@text.uri" },
+				Module = { icon = "", hl = "@namespace" },
+				Namespace = { icon = "", hl = "@namespace" },
+				Package = { icon = "", hl = "@namespace" },
+				Class = { icon = "𝓒", hl = "@type" },
+				Method = { icon = "ƒ", hl = "@method" },
+				Property = { icon = "", hl = "@method" },
+				Field = { icon = "", hl = "@field" },
+				Constructor = { icon = "", hl = "@constructor" },
+				Enum = { icon = "ℰ", hl = "@type" },
+				Interface = { icon = "ﰮ", hl = "@type" },
+				Function = { icon = "", hl = "@function" },
+				Variable = { icon = "", hl = "@constant" },
+				Constant = { icon = "", hl = "@constant" },
+				String = { icon = "𝓐", hl = "@string" },
+				Number = { icon = "#", hl = "@number" },
+				Boolean = { icon = "⊨", hl = "@boolean" },
+				Array = { icon = "", hl = "@constant" },
+				Object = { icon = "⦿", hl = "@type" },
+				Key = { icon = "🔐", hl = "@type" },
+				Null = { icon = "NULL", hl = "@type" },
+				EnumMember = { icon = "", hl = "@field" },
+				Struct = { icon = "𝓢", hl = "@type" },
+				Event = { icon = "🗲", hl = "@type" },
+				Operator = { icon = "+", hl = "@operator" },
+				TypeParameter = { icon = "𝙏", hl = "@parameter" },
+				Component = { icon = "", hl = "@function" },
+				Fragment = { icon = "", hl = "@constant" },
+			},
+		},
+	},
 	-- Git
 	{
 		"lewis6991/gitsigns.nvim",
