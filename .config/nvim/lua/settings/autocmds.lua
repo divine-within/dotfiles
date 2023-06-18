666d06f03765e4195d99e67d8a3b01a9c973f824
divine-yume <13299392+divine-yume@users.noreply.github.com>
2023-06-18 15:17:29 +0200

    Removal: Clipboard autocmd
    Not needed anymore

--- .config/nvim/lua/settings/autocmds.lua
+++ .config/nvim/lua/settings/autocmds.lua
@@ -1,32 +1,24 @@
 -- Colorscheme settings
 vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = {
-		"*",
-	},
-	callback = function()
-		vim.cmd("highlight BufDimText guibg='NONE' guifg=darkgrey guisp=darkgrey gui='NONE'")
-	end,
+    pattern = {
+        "*",
+    },
+    callback = function()
+        vim.cmd("highlight BufDimText guibg='NONE' guifg=darkgrey guisp=darkgrey gui='NONE'")
+    end,
 })
-
--- Yank to clipboard
-if vim.fn.has("wsl") == 0 then
-	vim.api.nvim_create_autocmd("TextYankPost", {
-		pattern = "*",
-		command = "call system('echo '.shellescape(join(v:event.regcontents, \"\\<CR>\")).')",
-	})
-end
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+    pattern = "*",
+    command = "set nopaste",
 })
 
 -- Treesitteer folding workaround
 vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter" }, {
-	group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
-	callback = function()
-		vim.opt.foldmethod = "expr"
-		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-	end,
+    group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
+    callback = function()
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    end,
 })
