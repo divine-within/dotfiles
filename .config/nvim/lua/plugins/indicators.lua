ff9dc172e0ad6e6910ba67f3606ebf3c6173f992
divine-yume <13299392+divine-yume@users.noreply.github.com>
2023-10-10 18:23:28 +0200

    Fix: Indicators

--- .config/nvim/lua/plugins/indicators.lua
+++ .config/nvim/lua/plugins/indicators.lua
@@ -1,96 +1,94 @@
 return {
-	-- Treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		event = "BufReadPost",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = {
-					"yaml",
-					"javascript",
-					"typescript",
-					"tsx",
-					"json",
-					"html",
-					"css",
-					"python",
-					"lua",
-					"fish",
-					"go",
-					"gomod",
-					"gowork",
-					"dockerfile",
-					"markdown",
-					"markdown_inline",
-				},
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = "BufReadPost",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "yaml",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "json",
+                    "html",
+                    "css",
+                    "python",
+                    "lua",
+                    "fish",
+                    "go",
+                    "gomod",
+                    "gowork",
+                    "dockerfile",
+                    "markdown",
+                    "markdown_inline",
+                },
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
+                highlight = {
+                    -- `false` will disable the whole extension
+                    enable = true,
+                },
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            })
+        end,
+    },
 
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
+    -- word usage highlighter
+    {
+        "RRethy/vim-illuminate",
+        lazy = true,
+        config = function()
+            require("illuminate").configure({
+                filetypes_denylist = {},
+            })
+        end,
+    },
 
-				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    -- jump to word indictors
+    {
+        "jinh0/eyeliner.nvim",
+        config = function()
+            require("eyeliner").setup({
+                highlight_on_key = true,
+                dim = true,
+            })
 
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
-				},
+            vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#900043", bold = true, underline = true })
+            vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#692047", underline = true })
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = {
+                    "*",
+                },
+                callback = function()
+                    vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#900043", bold = true, underline = true })
+                    vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#692047", underline = true })
+                end,
+            })
+        end,
+    },
 
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			})
-		end,
-	},
+    -- cursor movement highlighter
+    "DanilaMihailov/beacon.nvim",
 
-	-- word usage highlighter
-	{
-		"RRethy/vim-illuminate",
-		lazy = true,
-		config = function()
-			require("illuminate").configure({
-				filetypes_denylist = {},
-			})
-		end,
-	},
+    -- highlight yanked region
+    "machakann/vim-highlightedyank",
 
-	-- jump to word indictors
-	{
-		"jinh0/eyeliner.nvim",
-		config = function()
-			require("eyeliner").setup({
-				highlight_on_key = true,
-				dim = true,
-			})
-
-			vim.api.nvim_set_hl(0, "EyelinerPrimary", { underline = true })
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				pattern = {
-					"*",
-				},
-				callback = function()
-					vim.api.nvim_set_hl(0, "EyelinerPrimary", { underline = true })
-				end,
-			})
-		end,
-	},
-
-	-- cursor movement highlighter
-	"DanilaMihailov/beacon.nvim",
-
-	-- highlight yanked region
-	"machakann/vim-highlightedyank",
-
-	-- suggest mappings
-	{
-		"folke/which-key.nvim",
-		config = true,
-	},
+    -- suggest mappings
+    {
+        "folke/which-key.nvim",
+        config = true,
+    },
 }
